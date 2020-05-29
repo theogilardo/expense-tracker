42c68cc034d2fb53f2d6305b6f3b85f13bfd6001
theogilardo <gilardotheo@gmail.com>
2020-05-29 13:19:39 +0200

    new edit feature added

--- script.js
+++ script.js
@@ -1,178 +1,198 @@
-
-const textInput = document.getElementById('text-input');
-const amountInput = document.getElementById('amount-input');
-const addBtn = document.getElementById('add-btn');
-const transactionList = document.getElementById('transaction-list');
-const balance = document.getElementById('balance');
+// Expense Tracker UI
+const text = document.getElementById('text');
+const amount = document.getElementById('amount');
 const income = document.getElementById('income');
 const expense = document.getElementById('expense');
-const clear = document.getElementById('clear');
-const deleteItem = document.getElementById('delete');
+const listContainer = document.getElementById('list');
+const addBtn = document.getElementById('add-btn');
+
+// Popup
+const textUpdate = document.getElementById('text-update');
+const amountUpdate = document.getElementById('amount-update');
+const btnGoBack = document.getElementById('btn-go-back');
+const editPopupBtn = document.getElementById('edit-popup-btn');
+const popup = document.getElementById('popup');
 
 
-// Refactor iteration for items
-// Add Delete button for item
-//X Add Clear All
-// Work on the design + animations
+// Add percentage
+// Add update
+// Add more design + animations
 
+// let transactions = [
+//   { text: 'cereal', amount: 400, id: 30 },
+//   { text: 'home', amount: -1400, id: 35 },
+//   { text: 'salary', amount: 3400, id: 3 }
+// ]
 
-let data = {
-  allItems: {
-      inc: [],
-      exp: []
-  },
+let transactions = getData()
 
-  totals: {
-      inc:0,
-      exp:0
-  }
-};
+function init() {
+  clearDOM();
+  clearDataSet();
+  updateValues();
+  transactionsDOM(transactions);
 
-const proofData = getData();
+}
+
+init();
+
+function transactionsDOM(transactions) {
+  transactions.forEach(transaction => addToDOM(transaction));
+}
+
+function clearDataSet() {
+  localStorage.clear();
+  localStorage.setItem('transactions', JSON.stringify(transactions));
+}
+
+function clearDOM() {
+  listContainer.innerHTML = '';
+  text.innerHTML = '';
+  amount.innerHTML = '';
+}
 
 function getData() {
-  const transactionData = JSON.parse(localStorage.getItem('data'))
-  return transactionData === null ? data : transactionData
+  const transData = JSON.parse(localStorage.getItem('transactions'));
+  return transData === null ? [] : transData;
+};
+
+function getRandomId() {
+  return Math.floor(Math.random() * 1000000);
+};
+
+function addTransaction(e) {
+
+e.preventDefault();
+// Create the instance
+  const transaction = {
+    text: text.value,
+    amount: parseInt(amount.value),
+    id: getRandomId()
+  };
+
+// Push the instance to the transactions array
+  transactions.push(transaction)
+
+// Add the transaction to the DOM
+addToDOM(transaction);
+// Update the values on top for total/inc/exp
+updateValues();
+// Save to local storage
+localStorage.setItem('transactions', JSON.stringify(transactions));
+// Reset input
+text.innerHTML = '';
+amount.innerHTML = '';
+
 }
 
-function displayData(dataEl) {
+function updateValues() {
 
-  // Display updated Budget + Inc/Exp with iteration
-  const totalInc = dataEl.totals.inc;
-  const totalExp = dataEl.totals.exp;
+  // Compute balance, income and expense
+  const amountArr = transactions.map(transaction => transaction.amount);
+  const total = amountArr.reduce((acc, cur) => acc += cur, 0).toFixed(2);
 
-  balance.textContent = `$${totalInc + totalExp}`
-  income.textContent = `$${totalInc}`
-  expense.textContent = `$${totalExp}`
+  const inc = transactions
+    .filter(transaction => transaction.amount > 0)
+    .map(transaction => transaction.amount)
+    .reduce((acc, cur) => acc += cur, 0)
+    .toFixed(2);
 
+    const exp = transactions
+      .filter(transaction => transaction.amount < 0)
+      .map(transaction => transaction.amount)
+      .reduce((acc, cur) => acc += cur, 0)
+      .toFixed(2);
 
-  dataEl.allItems.inc.forEach(element => {
-
-         // Create div element
-         const transEl = document.createElement('div')
-
-         // Add a style class to it
-         transEl.classList.add('transaction-item')
-
-         // Set style depending on transaction
-             transEl.classList.add('income-item')
-
-         // Add its html content
-         transEl.innerHTML = `
-             <p class="transaction-name" id="transaction-name">${element.text}</p>
-             <p class="transaction-amount" id="transaction-amount">$${element.amount}</p>
-         `
-         // Place it inside transList div
-         transactionList.appendChild(transEl);
-  });
-
-  dataEl.allItems.exp.forEach(element => {
-
-         // Create div element
-         const transEl = document.createElement('div')
-
-         // Add a style class to it
-         transEl.classList.add('transaction-item')
-
-         // Set style depending on transaction
-             transEl.classList.add('expense-item')
-
-         // Add its html content
-         transEl.innerHTML = `
-             <p class="transaction-name" id="transaction-name">${element.text}</p>
-             <p class="transaction-amount" id="transaction-amount">$${element.amount}</p>
-         `
-         // Place it inside transList div
-         transactionList.appendChild(transEl);
-  });
+  // Render balance, inc and exp in DOM
+  balance.innerHTML = `$${total}`
+  income.innerHTML = `$${inc}`
+  expense.innerHTML = `$${exp}`
 
 };
 
-displayData(proofData);
+function addToDOM(transaction) {
 
-const addCart = function(text, amount) {
-  this.text = text,
-  this.amount = amount
-  // this.amount = parseInt(amount)
-}
+  const type = transaction.amount > 0 ? 'inc' : 'exp'
 
-function clearAll() {
+  // Create div
+  const newEl = document.createElement('div')
 
-  localStorage.clear();
-  // displayData(proofData);
-  window.location.reload();
+  // Add inc or exp class to div
+  newEl.classList.add(type)
+  // Add the inner HTML content
+  newEl.innerHTML = `
+    <p>${transaction.text}</p>
+    <p>$${transaction.amount}</p>
+    <button class="btn-delete" onclick="removeTransaction(${transaction.id})">x</button>
+    <button class="btn-edit" onclick="addPopup(${transaction.id})">Edit</button>
+  `
+  // Append element to the parent div
+  listContainer.appendChild(newEl)
+
 };
 
-// Click events
+function removeTransaction(id) {
 
-clear.addEventListener('click', clearAll);
+  // Remove it from transactions with filter function for ID
+  transactions = transactions.filter(transaction => transaction.id !== id)
+  // Refresh data + DOM with init
+  init();
+};
 
-addBtn.addEventListener('click', addNewItem);
 
-document.addEventListener('keyup', e => {
-  if (e.keyCode === 13) {
-    addNewItem();
-  }
-});
+// POPUP
 
-// Add function
-function addNewItem() {
+function addPopup(id){
 
-  const text = textInput.value;
-  const amount = parseInt(amountInput.value);
-  const newTrans = new addCart(text, amount)
+  // Make popup appear
+  popup.classList.toggle('hidden');
+  popup.setAttribute("data-id", `${id}`)
 
-  if (text === '' || amount === '') {
-      alert('Please enter a value!')
-    } else {
+  // Select transaction from dataset
+  const transaction = transactions.filter(transaction => transaction.id === id)
 
-    // Store data in the array for Items and Budget Balance
-    if (amount > 0) {
-      proofData.allItems.inc.push(newTrans);
-      proofData.totals.inc += newTrans.amount;
-    } else {
-      proofData.allItems.exp.push(newTrans);
-      proofData.totals.exp += newTrans.amount;
-    }
+  // Create popup with new text, amount input and update button
+  textUpdate.value = `${transaction[0].text}`;
+  textUpdate.focus();
+  // textUpdate.value = transaction.text;
+  amountUpdate.value = `${transaction[0].amount}`;
 
-    // Save it in the database
-    localStorage.setItem('data', JSON.stringify(proofData));
+  };
 
-    // Display Item
+function editTransaction(e){
 
-      // Create div element
-      const transEl = document.createElement('div')
+  // Retrieve ID from DOM
+  const retrieveID = parseInt(popup.dataset.id);
 
-      // Add a style class to it
-      transEl.classList.add('transaction-item')
+  //Find the right transaction with id and update it
+  findTransaction(retrieveID)[0].text = textUpdate.value;
+  findTransaction(retrieveID)[0].amount = parseInt(amountUpdate.value);
 
-      // Set style depending on transaction
-      if (amount > 0) {
-          transEl.classList.add('income-item')
-      } else {
-          transEl.classList.add('expense-item')
-      }
+  // Refresh Data Set
+  init();
 
-      // Add its html content
-      transEl.innerHTML = `
-          <p class="transaction-name" id="transaction-name">${text}</p>
-          <p class="transaction-amount" id="transaction-amount">$${amount}</p>
-      `
-      // Place it inside transList div
-      transactionList.appendChild(transEl);
+};
 
-      // Clear input
-      textInput.value = '';
-      amountInput.value = '';
+function findTransaction(id){
+  return transactions.filter(transaction => transaction.id === id)
+};
 
-    // Display updated Budget + Inc/Exp
-    const totalInc = proofData.totals.inc;
-    const totalExp = proofData.totals.exp;
 
-    balance.textContent = `$${totalInc + totalExp}`
-    income.textContent = `$${totalInc}`
-    expense.textContent = `$${totalExp}`
+// Event Listeners
 
-  }
-}
+addBtn.addEventListener('click', addTransaction);
+editPopupBtn.addEventListener('click', editTransaction)
+btnGoBack.addEventListener('click', () => popup.classList.toggle('hidden'));
+
+
+
+
+
+
+
+
+
+
+
+
 
